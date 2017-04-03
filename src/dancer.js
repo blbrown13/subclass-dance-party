502f000f13cd1784ed7a85946c1c5bdebc45261b
Hack Reactor Student <student@hackreactor.com>
2017-04-03 16:47:29 -0700

    Refactor dancer and blinkyDancer to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -30,3 +30,24 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+
+var Dancer = function(top, left, timeBetweenSteps) {
+  this.$node = $('<span class="dancer"></span>');
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.step();
+};
+
+Dancer.prototype.step = function() {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+
+  this.$node.css(styleSettings);
+};
